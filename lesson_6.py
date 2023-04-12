089dc290e5b9baca844f204f9babd096e9544398
MorevDA <morevda1983@gmail.com>
2023-04-12 22:21:31 +0300

    Скорректирована разметка, удалены лишние пустые строки

--- lesson_6.py
+++ lesson_6.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 from time import sleep
-
-
 
 link = 'https://suninjuly.github.io/math.html'
 
@@ -15,7 +12,7 @@ try:
     people_radio = browser.find_element(By.CSS_SELECTOR, '.btn.btn-default')
     people_checked = people_radio.get_attribute('disabled')
     print("submit: ", people_checked)
-    #assert people_checked == "true", "People radio is not selected by default"
+    assert people_checked == "true", "People radio is not selected by default"
 finally:
     sleep(10)
     browser.quit()
